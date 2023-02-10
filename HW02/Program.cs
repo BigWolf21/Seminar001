34db94e8194700bf76e228924f9e76e27d7f6ee5
Your Name~git <you@example.com>
2023-02-10 23:55:09 +0700

    add HW02 и выполнено 2 задания

---
+++ HW02/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a, b, c, max;
+a = 2;
+Console.WriteLine(a);
+b = 3;
+Console.WriteLine(b);
+c = 7;
+Console.WriteLine(c);
+max = a;
+
+if(b > max)
+{
+  max = b;
+}
+if(c > max)
+{
+  max = c;
+}
+Console.WriteLine("max = " + max);
+
+a = 44;
+Console.WriteLine(a);
+b = 5;
+Console.WriteLine(b);
+c = 78;
+Console.WriteLine(c);
+max = a;
+
+if(b > max)
+{
+  max = b;
+}
+if(c > max)
+{
+  max = c;
+}
+Console.WriteLine("max = " + max);
+
+a = 22;
+Console.WriteLine(a);
+b = 3;
+Console.WriteLine(b);
+c = 9;
+Console.WriteLine(c);
+max = a;
+
+if(b > max)
+{
+  max = b;
+}
+if(c > max)
+{
+  max = c;
+}
+Console.WriteLine("max = " + max);
